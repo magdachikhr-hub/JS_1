a48ebc4366463a514ef57cdfbe7d08d6b0515bb7
Magda Chikhradze <magda292005@gmail.com>
2026-03-16 20:07:11 +0400

    perimeter and area

---
+++ main.js
@@ -0,0 +1,14 @@
+let width = 50;
+let height = 30;
+
+console.log(2 * (width + height));
+
+let perimeter = 2 * (width + height);
+
+console.log(perimeter);
+
+console.log(width * height);
+
+let area = width * height;
+
+console.log(area);
